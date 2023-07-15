45cc7091870b6f4744b6773cba9a4b61328c8ef8
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-07-15 11:14:16 +0200

    Update dependencies

--- unasyncd/cli.py
+++ unasyncd/cli.py
@@ -48,7 +48,7 @@ async def _run(*, config: Config, check_only: bool, verbose: bool) -> bool:
     return files_changed > 0
 
 
-@click.command()  # type: ignore[arg-type]
+@click.command()
 @click.option(
     "--cache/--no-cache",
     is_flag=True,
